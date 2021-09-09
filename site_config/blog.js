2d638592f3e6960f54483122f289371841d1d1e0
jinchen <jinchendut@outlook.com>
2021-09-09 14:35:05 +0800

    add blog: introduce edge autonomy and the design detail about the Yurthub

--- site_config/blog.js
+++ site_config/blog.js
@@ -24,6 +24,13 @@ export default {
         desc: 'By deploying the control plane of the Kubernetes cluster in the cloud and connecting the Raspberry Pi to the cluster to build a cloud management side scene,demonstrate the core capabilities of OpenYurt',
         link: '/zh-cn/blog/Play_with_Openyurt_on_Raspberry_Pi.html',
       },
+      {
+        title: 'Edge autonomy of OpenYurt: YurtHub Design Detail',
+        author: '新胜',
+        dateStr: 'June 18，2020',
+        desc: 'introduce of edge autonomy and the design detail about YurtHub that realizes the edge autonomy of OpenYurt',
+        link: '/zh-cn/blog/YurtHub_Design_Detail.html',
+      },
     ],
   },
   'zh-cn': {
@@ -40,7 +47,7 @@ export default {
       {
         title: 'OpenYurt 开箱测评 | 一键让原生 K8s 集群具备边缘计算能力',
         author: '郑超',
-        dateStr: 'june 6，2020',
+        dateStr: 'June 6，2020',
         desc: '如何使用 OpenYurt 提供的命令行管理工具 Yurtctl， 高效快速地部署 OpenYurt 集群',
         link: '/zh-cn/blog/use_of_yurtctl.html',
       },
@@ -50,6 +57,13 @@ export default {
         dateStr: 'January 1，2021',
         desc: '通过在云端部署 Kubernetes 集群的控制面，并将树莓派接入集群来搭建云管边场景,来演示 OpenYurt 的核心能力',
         link: '/zh-cn/blog/Play_with_Openyurt_on_Raspberry_Pi.html',
+      },
+      {
+        title: 'OpenYurt边缘自治能力的设计细节:YurtHub',
+        author: '新胜',
+        dateStr: 'June 18，2020',
+        desc: '介绍边缘自治特性以及OpenYurt实现边缘自治的设计细节',
+        link: '/zh-cn/blog/YurtHub_Design_Detail.html',
       },
     ],
   },
