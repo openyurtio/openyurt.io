732fe2bf926ae1c0d82fd0dd896fcddf7482f971
LofiSu <163713803+LofiSu@users.noreply.github.com>
2024-08-12 19:33:06 +0800

    [Feature]Add animation effects to  website for enhanced user experience (#442)
    * Add animation effects to  website for enhanced user experience

--- src/components/FeaturesList.js
+++ src/components/FeaturesList.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import HomePageFeatures from '../data/HomePageFeatures';
+import React from "react";
+import HomePageFeatures from "../data/HomePageFeatures";
 
 const FeatureBlock = () => {
-    return (
-        <div className="hero">
-            <div className="container">
-                <section className="features">
-                    <div className="container">
-                        {
-                            HomePageFeatures.map(({imgUrl, title, description, reverse}, index) => (
-                                <div key={index} className="row feature">
-                                    <div className="col col--3">
-                                        <div className="text--center">
-                                            {imgUrl && <img className="featureImage" src={imgUrl} alt={title} />}
-                                        </div>
-                                    </div>
-                                    <div className="col col--9 featureDesc">
-                                        <div>
-                                            <h2>{title}</h2>
-                                            <div>{description}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        }
+  return (
+    <div className="hero" data-aos="fade-up">
+      <div className="container">
+        <section className="features">
+          <div className="container">
+            {HomePageFeatures.map(
+              ({ imgUrl, title, description, reverse }, index) => (
+                <div
+                  key={index}
+                  className={`row feature ${reverse ? "reverse" : ""}`}
+                  data-aos="fade-up"
+                  data-aos-delay={index * 200} // Optional delay based on index
+                >
+                  <div className="col col--3" data-aos="fade-right">
+                    <div className="text--center">
+                      {imgUrl && (
+                        <img
+                          className="featureImage"
+                          src={imgUrl}
+                          alt={title}
+                        />
+                      )}
                     </div>
-                </section>
-            </div>
-        </div>
-    )
-}
+                  </div>
+                  <div className="col col--9 featureDesc" data-aos="fade-left">
+                    <div>
+                      <h2>{title}</h2>
+                      <div>{description}</div>
+                    </div>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
 
-export default FeatureBlock
+export default FeatureBlock;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,59 @@
-import React from 'react';
-import Layout from '@theme/Layout';
-import Translate, { translate } from '@docusaurus/Translate';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import FeatureBlock from '../components/FeaturesList';
-import WhatIs from '../components/WhatIs';
-import Button from '../components/Button';
-import GhButton from '../components/GitHubButton'
+import React, { useEffect } from "react";
+import Layout from "@theme/Layout";
+import Translate from "@docusaurus/Translate";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import FeatureBlock from "../components/FeaturesList";
+import WhatIs from "../components/WhatIs";
+import Button from "../components/Button";
+import GhButton from "../components/GitHubButton";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
-    const context = useDocusaurusContext();
-    const { siteConfig = {} } = context;
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
 
-    return (
-        <Layout title={siteConfig.tagline} description={siteConfig.tagline}>
-            <header className="hero">
-                <div className="container text--center">
-                    {/*<div className={styles.heroLogoWrapper}>*/}
-                    {/*    <img className={styles.heroLogo} src={useBaseUrl('img/openyrut.png')} alt="OpenYurt Logo" />*/}
-                    {/*</div>*/}
-                    <h2 className="hero__title">{siteConfig.title}</h2>
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // 动画持续时间
+      easing: "ease-in-out", // 动画的缓动函数
+      once: true, // 是否只在第一次滚动时触发动画
+    });
+  }, []);
 
-                    <GhButton />
+  return (
+    <Layout title={siteConfig.tagline} description={siteConfig.tagline}>
+      <header className="hero">
+        <div className="container text--center" data-aos="fade-up">
+          <h2 className="hero__title">{siteConfig.title}</h2>
+          <GhButton />
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className="heroButtons" data-aos="zoom-in">
+            <Button href={useBaseUrl("docs/installation/summary")}>
+              <Translate>Get Started</Translate>
+            </Button>
+            <Button href={useBaseUrl("docs/")}>
+              <Translate>Learn More</Translate>
+            </Button>
+          </div>
+        </div>
+      </header>
 
-                    <p className="hero__subtitle">{siteConfig.tagline}</p>
-                    <div
-                        className="heroButtons">
-                        <Button href={useBaseUrl('docs/installation/summary')}><Translate>Get Started</Translate></Button>
-                        <Button href={useBaseUrl('docs/')}><Translate>Learn More</Translate></Button>
-                    </div>
-                </div>
-            </header>
+      <WhatIs data-aos="fade-up" />
 
-            <WhatIs />
+      <FeatureBlock data-aos="fade-up" />
 
-            <FeatureBlock />
-
-            <div className="hero">
-                <div className="container text--center">
-                    <h3 className="hero__subtitle">
-                        <Translate>OpenYurt is a</Translate> <a href="https://cncf.io/">Cloud Native Computing Foundation</a> <Translate>sandbox project</Translate>
-                    </h3>
-                    <div className="cncf-logo" />
-                </div>
-            </div>
-        </Layout>
-    );
+      <div className="hero" data-aos="fade-up">
+        <div className="container text--center">
+          <h3 className="hero__subtitle">
+            <Translate>OpenYurt is a</Translate>{" "}
+            <a href="https://cncf.io/">Cloud Native Computing Foundation</a>{" "}
+            <Translate>sandbox project</Translate>
+          </h3>
+          <div className="cncf-logo" />
+        </div>
+      </div>
+    </Layout>
+  );
 }
